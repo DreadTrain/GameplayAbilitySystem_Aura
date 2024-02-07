ba48bf717face60f2bf654aaad34cffbf0fc8c92
Wayne Miller <waynesgames1234@outlook,com>
2024-02-07 03:16:11 -0400

    Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+// Copyright Mind Crawler Studio
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Mind Crawler Studio
+
+
+#include "Character/AuraEnemy.h"
+

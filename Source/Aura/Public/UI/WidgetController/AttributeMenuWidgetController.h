66db0e23752f3e1e36f9ca645587dc7beda138d3
Wayne Miller <waynesgames1234@outlook,com>
2024-02-15 00:27:16 -0400

    Responding to Attribute Changes

--- Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/AttributeMenuWidgetController.h
@@ -28,4 +28,7 @@ protected:
 
 	UPROPERTY(EditAnywhere)
 	TObjectPtr<UAttributeInfo> AttributeInfo;
+
+private:
+	void BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const;
 };

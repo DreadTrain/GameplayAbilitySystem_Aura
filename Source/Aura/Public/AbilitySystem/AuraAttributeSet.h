6ac82e4182fd0fd4dc2325fc2b02b6d2c17a914c
Wayne Miller <waynesgames1234@outlook,com>
2024-02-18 00:28:09 -0400

    Set Caller Magnitude

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -144,8 +144,15 @@ public:
 	UPROPERTY(BlueprintReadOnly, ReplicatedUsing = OnRep_Mana, Category = "Vital Attributes")
 	FGameplayAttributeData Mana;
 	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, Mana);
-	
 
+	/*
+	 * Meta Attribute Set
+	 */
+	
+    UPROPERTY(BlueprintReadOnly, Category = "Meta Attributes")
+	FGameplayAttributeData IncomingDamage;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet, IncomingDamage);
+	
 	// Vital Attributes Below
 	
     UFUNCTION()

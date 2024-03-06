a6a1e6d78a8eb0f535820c8384b79c4c57ee1c23
Wayne Miller <waynesgames1234@outlook,com>
2024-03-06 00:13:39 -0400

    Spell Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Mind Crawler Studio
+
+
+#include "UI/WidgetController/SpellMenuWidgetController.h"
+
+void USpellMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
+void USpellMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}

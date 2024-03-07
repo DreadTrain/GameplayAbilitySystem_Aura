66b00478d1934994c0eb92c68109df8604bf7cda
Wayne Miller <waynesgames1234@outlook,com>
2024-03-06 22:59:41 -0400

    Ability Level Requirement

--- Source/Aura/Private/AuraGameplayTags.cpp
+++ Source/Aura/Private/AuraGameplayTags.cpp
@@ -227,6 +227,11 @@ GameplayTags.Attributes_Resistance_Fire = UGameplayTagsManager::Get().AddNativeG
 		FString("FireBolt Ability Tag")
 		);
 
+	GameplayTags.Abilities_Lightning_Electrocute = UGameplayTagsManager::Get().AddNativeGameplayTag(
+		FName("Abilities.Lightning.Electrocute"),
+		FString("Electrocute Ability Tag")
+		);
+
 	GameplayTags.Abilities_HitReact = UGameplayTagsManager::Get().AddNativeGameplayTag(
 		FName("Abilities.HitReact"),
 		FString("Hit React Ability")

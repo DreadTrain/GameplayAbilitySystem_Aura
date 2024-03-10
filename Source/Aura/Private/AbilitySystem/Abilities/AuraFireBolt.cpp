b474c5b13e866d4fd4564f9713c83ec212132907
Wayne Miller <waynesgames1234@outlook,com>
2024-03-09 22:35:11 -0400

    Cost and Cooldown in Spell Description

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -0,0 +1,101 @@
+// Copyright Mind Crawler Studio
+
+
+#include "AbilitySystem/Abilities/AuraFireBolt.h"
+#include "Aura/Public/AuraGameplayTags.h"
+
+FString UAuraFireBolt::GetDescription(int32 Level)
+{
+	
+	const int32 Damage = GetDamageByDamageType(Level, FAuraGameplayTags::Get().Damage_Fire);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCoolDown(Level);
+	if (Level == 1)
+	{
+		return FString::Printf(TEXT(
+			// Title 
+			"<Title>FIRE BALL</>\n\n"
+
+			// Level
+			"<Small>Level: </><Level>%d</>\n"
+			// ManaCost
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			// Cooldown
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+			
+			"<Default>Launches a ball of fire, "
+			"exploding on impact and dealing: </>"
+
+			// Damage
+			"<Damage>%d</><Default> fire damage with"
+			
+			" a chance to burn</>"),
+
+			// Values
+			Level,
+			ManaCost,
+			Cooldown,
+			Damage);
+			
+	}
+	else
+	{
+		return FString::Printf(TEXT(
+			// Title 
+			"<Title>FIRE BALL</>\n\n"
+
+			// Level
+			"<Small>Level: </><Level>%d</>\n"
+			// ManaCost
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			// Cooldown
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+			// Number of FireBalls
+			"<Default>Launches %d balls of fire, "
+			"exploding on impact and dealing: </>"
+
+			// Damage
+			"<Damage>%d</><Default> fire damage with"
+			" a chance to burn</>"),
+
+			// Values
+			Level,
+			ManaCost,
+			Cooldown,
+			FMath::Min(Level, NumProjectiles),
+			Damage);
+	}
+}
+
+FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
+{
+	const int32 Damage = GetDamageByDamageType(Level, FAuraGameplayTags::Get().Damage_Fire);
+	const float ManaCost = FMath::Abs(GetManaCost(Level));
+	const float Cooldown = GetCoolDown(Level);
+	return FString::Printf(TEXT(
+			// Title 
+			"<Title>NEXT LEVEL: </>\n\n"
+
+			// Level
+			"<Small>Level: </><Level>%d</>\n"
+			// ManaCost
+			"<Small>ManaCost: </><ManaCost>%.1f</>\n"
+			// Cooldown
+			"<Small>Cooldown: </><Cooldown>%.1f</>\n\n"
+
+			// Number of FireBalls
+			"<Default>Launches %d balls of fire, "
+			"exploding on impact and dealing: </>"
+
+			// Damage
+			"<Damage>%d</><Default> fire damage with"
+			" a chance to burn</>"),
+
+			// Values
+			Level,
+			ManaCost,
+			Cooldown,
+			FMath::Min(Level, NumProjectiles),
+			Damage);
+}
